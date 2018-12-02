95568ecd39961ac547546186fc8647c989b3e95f
Darragh Hurley <hurleyd4@tcd.ie>
2018-12-02 21:48:49 +0000

    removed list 2

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,4 @@ import stdio
 # Write 'Hello, World' to standard output.
 stdio.writeln('Hello, World')
 thislist = ["apple", "banana", "cherry","donut","elephant"]
-secondlist = ["one", "two", "three","four","five"]
 print(thislist)
-print(secondlist)
