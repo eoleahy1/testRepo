55369c2c1464ab88d6e2f24214a039cb3c11d2be
Darragh Hurley <hurleyd4@tcd.ie>
2018-12-02 21:48:04 +0000

    adding hellowrold.py

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+import stdio
+
+# Write 'Hello, World' to standard output.
+stdio.writeln('Hello, World')
+thislist = ["apple", "banana", "cherry","donut","elephant"]
+secondlist = ["one", "two", "three","four","five"]
+print(thislist)
+print(secondlist)
